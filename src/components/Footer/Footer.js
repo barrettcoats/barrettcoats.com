fa3673c0ca73bec0924b54b8806d985fbd2ee2ec
J. Barrett Coats <barrett.coats@gmail.com>
2024-09-29 18:39:39 -0500

    Add horizontal rule to the footer
    Added a horizontal rule to the footer for better separation of the
    content and footer sections of the page.

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -7,39 +7,42 @@ import LinkedInLogo from "./assets/linkedin.svg";
 const Footer = () => {
   return (
     <footer>
-      <div class="footer__section footer__section--quicklinks">
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/notes">Notes</Link>
-          </li>
-          <li>
-            <Link to="/photos">Photos</Link>
-          </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-        </ul>
-      </div>
-      <div class="footer__section footer__section--external-icons">
-        <ul>
-          <li>
-            <a href="https://github.com/barrettcoats">
-              <GithubLogo />
-            </a>
-          </li>
-          <li>
-            <a href="https://www.linkedin.com/in/barrett-coats-0557852a9/">
-              <LinkedInLogo />
-            </a>
-          </li>
-        </ul>
-      </div>
-      <div class="footer__section">
-        Contact:{" "}
-        <a href="mailto:hello@barrettcoats.com">hello@barrettcoats.com</a>
+      <hr />
+      <div class="footer-container">
+        <div class="footer-container__section footer-container__section--quicklinks">
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/notes">Notes</Link>
+            </li>
+            <li>
+              <Link to="/photos">Photos</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+          </ul>
+        </div>
+        <div class="footer-container__section footer-container__section--external-icons">
+          <ul>
+            <li>
+              <a href="https://github.com/barrettcoats">
+                <GithubLogo />
+              </a>
+            </li>
+            <li>
+              <a href="https://www.linkedin.com/in/barrett-coats-0557852a9/">
+                <LinkedInLogo />
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div class="footer-container__section">
+          Contact:{" "}
+          <a href="mailto:hello@barrettcoats.com">hello@barrettcoats.com</a>
+        </div>
       </div>
     </footer>
   );
